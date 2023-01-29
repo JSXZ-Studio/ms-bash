e0087e9633c71e26fc3332627534c4e3daeaf8ea
xuguanqun <33191843+xuguanqun@users.noreply.github.com>
2023-01-29 23:13:55 +0800

    Update create-environment.sh

--- create-environment.sh
+++ create-environment.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello World!!"
+echo "安装 Docker"
+
+yum -y install docker
+
+# 添加docker加速器
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://qtmx3fa5.mirror.aliyuncs.com"]
+}
+
+# 重启服务
+sudo systemctl daemon-reload
+sudo systemctl restart docker
